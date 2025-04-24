33f1f2c4fd09800ea55b936645f204aadd38bd19
sepulvedalili <lilibethdelvalle17@gmail.com>
2025-04-25 01:47:06 +0200

    feat: set up initial app structure

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-green-600 p-4 text-white">
+            MOOD
+        </nav>
+    )
+}
---
+++ frontend/src/pages/MoodEntryPage.jsx
@@ -0,0 +1,3 @@
+export default function MoodEntryPage() {
+    return <h1 className="text-3xl font-bold">Moood Entry page</h1>;
+}
